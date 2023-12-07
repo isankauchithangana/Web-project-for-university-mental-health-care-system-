6504f71496ad6bfbd662ce6757ab45ceeb091a32
ChanakaThushan <chanakab468@gmail.com>
2023-12-07 22:45:36 +0530

    aded new thing to admin_index page

--- admin_index.php
+++ admin_index.php
@@ -23,6 +23,30 @@
             <input type="text" name="lname" placeholder="Last name" required>
           </div>
         </div>
+
+        <div class="field input">
+          <label>Email Address</label>
+          <input type="text" name="email" placeholder="Enter your email" required>
+        </div>
+        <div class="field input">
+          <label>Password</label>
+          <input type="password" name="password" placeholder="Enter new password" required>
+          <i class="fas fa-eye"></i>
+        </div>
+        <div class="field image">
+          <label>Select Image</label>
+          <input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jpg">
+        </div>
+        <div class="field button">
+          <input type="submit" name="submit" value="Continue to Chat">
+        </div>
+      </form>
+      <div class="link">Already signed up? <a href="admin_login.php">Login now</a></div>
+    </section>
+  </div>
+
+  <script src="admin-javascript/pass-show-hide.js"></script>
+  <script src="admin-javascript/signup.js"></script>
         
 </body>
 </html>
